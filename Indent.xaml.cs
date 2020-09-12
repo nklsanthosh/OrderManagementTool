e285d3460661d2c301fe92a49f5df032dc110544
nklsanthosh <nklsanthosh143@gmail.com>
2020-09-12 16:40:00 +0530

    added update field

--- Indent.xaml.cs
+++ Indent.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Shapes;
 using System.Linq;
 using Caliburn.Micro;
 using OrderManagementTool.Models.Indent;
+using System.Data;
 
 namespace OrderManagementTool
 {
@@ -21,79 +22,54 @@ namespace OrderManagementTool
     /// </summary>
     public partial class Indent : Window
     {
-
-        // private readonly List<string> itemName;
-
         private List<string> itemName;
         private List<string> itemCode;
         private string selectedItemCode;
         private List<string> units;
         private string selectedItemName;
         private int quantityEntered;
+        private int gridSelectedIndex;
         OrderManagementContext orderManagementContext = new OrderManagementContext();
         private List<GridIndent> gridIndents = new List<GridIndent>();
         public BindableCollection<string> ItemName { get; set; }
         public Indent()
         {
             InitializeComponent();
+            LoadItemName();
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //DataRowView rowview = grid_indentdata.SelectedItem as DataRowView;
+            var rowview = grid_indentdata.SelectedItem as GridIndent;
+            gridSelectedIndex = grid_indentdata.SelectedIndex;
 
-        }
-
-        private void cbx_itemname_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            itemName = (from i in orderManagementContext.ItemCategory
-                        select i.ItemCategoryName).Distinct().ToList();
-            foreach (var i in itemName)
+            if (rowview != null)
             {
-                cbx_itemname.Items.Add(i.Trim());
+                txt_quantity.Text = Convert.ToInt32(rowview.Quantity).ToString();
+                LoadItemName();
+                cbx_itemname.SelectedItem = rowview.ItemName;
+                LoadItemCode();
+                cbx_itemcode.SelectedItem = rowview.ItemCode;
+                LoadDesctiption();
+                LoadUnits();
+                cbx_units.SelectedItem = rowview.Units;
             }
         }
 
         private void cbx_itemname_DropDownOpened(object sender, EventArgs e)
         {
-            cbx_itemname.Items.Clear();
-            var itemName = (from i in orderManagementContext.ItemCategory
-                            select i.ItemCategoryName).Distinct().ToList();
-            foreach (var i in itemName)
-            {
-                cbx_itemname.Items.Add(i.Trim());
-            }
+            LoadItemName();
         }
 
         private void cbx_itemcode_DropDownOpened(object sender, EventArgs e)
         {
-            if (cbx_itemname.SelectedItem != null)
-            {
-                selectedItemName = cbx_itemname.SelectedItem.ToString();
-                cbx_itemcode.Items.Clear();
-                itemCode = (from i in orderManagementContext.ItemMaster
-                            from ic in orderManagementContext.ItemCategory
-                            where i.ItemCategoryId == ic.ItemCategoryId && ic.ItemCategoryName == selectedItemName
-                            select i.ItemCode).Distinct().ToList();
-                foreach (var i in itemCode)
-                {
-                    cbx_itemcode.Items.Add(i);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Please Select ItemName");
-            }
+            LoadItemCode();
         }
 
         private void cbx_units_DropDownOpened(object sender, EventArgs e)
         {
-            cbx_units.Items.Clear();
-            units = (from u in orderManagementContext.UnitMaster
-                     select u.Unit).Distinct().ToList();
-            foreach (var u in units)
-            {
-                cbx_units.Items.Add(u.Trim());
-            }
+            LoadUnits();
         }
 
         private void txt_quantity_lostfocus(object sender, RoutedEventArgs e)
@@ -110,38 +86,44 @@ namespace OrderManagementTool
 
         private void cbx_itemcode_DropDownClosed(object sender, EventArgs e)
         {
-            selectedItemCode = cbx_itemcode.SelectionBoxItem.ToString();
-            var itemDetails = (from i in orderManagementContext.ItemMaster
-                               from ic in orderManagementContext.ItemCategory
-                               where i.ItemCategoryId == ic.ItemCategoryId && ic.ItemCategoryName == selectedItemName && i.ItemCode == selectedItemCode
-                               select i).FirstOrDefault();
-            if (itemDetails != null)
-            {
-                txt_description.Text = itemDetails.Description;
-                txt_technical_description.Text = itemDetails.TechnicalSpecification;
-            }
+            LoadDesctiption();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (cbx_itemname.SelectedItem != null && cbx_itemcode.SelectedItem != null && txt_quantity.Text != "" 
+            if (cbx_itemname.SelectedItem != null && cbx_itemcode.SelectedItem != null && txt_quantity.Text != ""
                && quantityEntered != 0 && cbx_units.SelectedItem != null)
             {
-                
+                bool itemPresent = false;
                 GridIndent gridIndent = new GridIndent();
-                gridIndent.SlNo = gridIndents.Count+1;
+                gridIndent.SlNo = gridIndents.Count + 1;
                 gridIndent.ItemCode = cbx_itemcode.SelectedItem.ToString();
                 gridIndent.ItemName = cbx_itemname.SelectedItem.ToString();
                 gridIndent.Quantity = quantityEntered;
                 gridIndent.Technical_Specifications = txt_technical_description.Text.Trim();
                 gridIndent.Units = cbx_units.SelectedItem.ToString();
-                gridIndent.Remarks = Txt_remarks.Text.Trim();
-                gridIndents.Add(gridIndent);
-                grid_indentdata.ItemsSource = null;
-                grid_indentdata.ItemsSource = gridIndents;
-                
-                
+                gridIndent.Remarks = txt_remarks.Text.Trim();
 
+                foreach (var i in gridIndents)
+                {
+                    if (i.ItemName == gridIndent.ItemName && i.ItemCode == gridIndent.ItemCode && i.Description == gridIndent.Description && i.Technical_Specifications == gridIndent.Technical_Specifications && itemPresent == false)
+                    {
+                        itemPresent = true;
+
+                    }
+                }
+                if (!itemPresent)
+                {
+                    gridIndents.Add(gridIndent);
+                    grid_indentdata.ItemsSource = null;
+                    grid_indentdata.ItemsSource = gridIndents;
+                    ClearFields();
+                }
+                else
+                {
+                    MessageBox.Show("Item Already Present");
+                }
             }
             else if (cbx_itemname.SelectedItem == null)
             {
@@ -162,6 +144,117 @@ namespace OrderManagementTool
             else if (cbx_units.SelectedItem == null)
             {
                 MessageBox.Show("Please Select Units");
+            }
+        }
+
+        private void LoadItemName()
+        {
+            cbx_itemname.Items.Clear();
+            var itemName = (from i in orderManagementContext.ItemCategory
+                            select i.ItemCategoryName).Distinct().ToList();
+            foreach (var i in itemName)
+            {
+                cbx_itemname.Items.Add(i.Trim());
+            }
+        }
+        private void LoadDesctiption()
+        {
+            selectedItemCode = cbx_itemcode.SelectionBoxItem.ToString();
+            var itemDetails = (from i in orderManagementContext.ItemMaster
+                               from ic in orderManagementContext.ItemCategory
+                               where i.ItemCategoryId == ic.ItemCategoryId && ic.ItemCategoryName == selectedItemName && i.ItemCode == selectedItemCode
+                               select i).FirstOrDefault();
+            if (itemDetails != null)
+            {
+                txt_description.Text = itemDetails.Description;
+                txt_technical_description.Text = itemDetails.TechnicalSpecification;
+            }
+        }
+        private void LoadUnits()
+        {
+            cbx_units.Items.Clear();
+            units = (from u in orderManagementContext.UnitMaster
+                     select u.Unit).Distinct().ToList();
+            foreach (var u in units)
+            {
+                cbx_units.Items.Add(u.Trim());
+            }
+        }
+        private void LoadItemCode()
+        {
+            if (cbx_itemname.SelectedItem != null)
+            {
+                selectedItemName = cbx_itemname.SelectedItem.ToString();
+                cbx_itemcode.Items.Clear();
+                itemCode = (from i in orderManagementContext.ItemMaster
+                            from ic in orderManagementContext.ItemCategory
+                            where i.ItemCategoryId == ic.ItemCategoryId && ic.ItemCategoryName == selectedItemName
+                            select i.ItemCode).Distinct().ToList();
+                foreach (var i in itemCode)
+                {
+                    cbx_itemcode.Items.Add(i);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select ItemName");
+            }
+        }
+        private void ClearFields()
+        {
+            cbx_itemname.SelectedValue = "";
+            cbx_itemcode.SelectedValue = "";
+            txt_description.Text = "";
+            txt_quantity.Text = "";
+            txt_technical_description.Text = "";
+            cbx_units.SelectedValue = "";
+            txt_remarks.Text = "";
+            txt_technical_description.Text = "";
+        }
+
+        private void btn_clear_fields_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (gridSelectedIndex >= 0)
+            {
+                bool itemPresent = false;
+                GridIndent value = new GridIndent();
+                value.ItemCode = cbx_itemcode.SelectedItem.ToString();
+                value.ItemName = cbx_itemname.SelectedItem.ToString();
+                value.Quantity = quantityEntered;
+                value.Technical_Specifications = txt_technical_description.Text.Trim();
+                value.Units = cbx_units.SelectedItem.ToString();
+                value.Remarks = txt_remarks.Text.Trim();
+
+                foreach (var i in gridIndents)
+                {
+                    if (i.ItemName == value.ItemName && i.ItemCode == value.ItemCode && i.Description == value.Description && i.Technical_Specifications == value.Technical_Specifications && itemPresent == false)
+                    {
+                        itemPresent = true;
+                    }
+                }
+                if (!itemPresent)
+                {
+                    var gridIndent = gridIndents[gridSelectedIndex];
+                    gridIndent.ItemCode = cbx_itemcode.SelectedItem.ToString();
+                    gridIndent.ItemName = cbx_itemname.SelectedItem.ToString();
+                    gridIndent.Quantity = quantityEntered;
+                    gridIndent.Technical_Specifications = txt_technical_description.Text.Trim();
+                    gridIndent.Units = cbx_units.SelectedItem.ToString();
+                    gridIndent.Remarks = txt_remarks.Text.Trim();
+                    grid_indentdata.ItemsSource = null;
+                    grid_indentdata.ItemsSource = gridIndents;
+                    gridSelectedIndex = -1;
+                    ClearFields();
+                }
+                else
+                {
+                    MessageBox.Show("Item Already Present");
+                }
             }
         }
     }
