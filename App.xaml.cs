5b65ec8f7dc59345cfd8e60666d0b264727c9872
Dinesh <meetdineshv@gmail.com>
2020-09-15 12:22:23 +0530

    Added Splash Screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,36 @@ namespace OrderManagementTool
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //initialize the splash screen and set it as the application main window
+            var splashScreen = new SplashScreen();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            //in order to ensure the UI stays responsive, we need to
+            //do the work on a different thread
+            Task.Factory.StartNew(() =>
+            {
+                //simulate some work being done
+                System.Threading.Thread.Sleep(3000);
+
+                //since we're not on the UI thread
+                //once we're done we need to use the Dispatcher
+                //to create and show the main window
+                this.Dispatcher.Invoke(() =>
+                {
+                    //initialize the main window, set it as the application main window
+                    //and close the splash screen
+                    var loginWindow = new MainWindow();
+                    this.MainWindow = loginWindow;
+                    loginWindow.Show();
+                    splashScreen.Close();
+                });
+            });
+            
+        }
     }
 }
