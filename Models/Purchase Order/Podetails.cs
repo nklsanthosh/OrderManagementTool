7fab9770d1ffb661b34867ff31611c57d4ee97f1
nklsanthosh <nklsanthosh143@gmail.com>
2020-11-16 15:42:25 +0530

    Added Models for PO

---
+++ Models/Purchase Order/Podetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagementTool.Models.Purchase_Order
+{
+    public class Podetails
+    {
+        public List<Poitem> Poitems;
+    }
+}
---
+++ Models/Purchase Order/Poitem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagementTool.Models.Purchase_Order
+{
+    public class Poitem
+    {
+        public int Sl_NO { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int Units { get; set; }
+        public double Unit_Price { get; set; }
+        public double Total_Price { get; set; }
+    }
+}
